cf20af6dfcb2b220c711d2a5f4afc1dc8cb849e8
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-02-03 19:18:34 +0300

    Project Creation and testing of the message sending system
    Created Console for Publisher and Sucsriber and added RabbitMQ libraries. Successfully connected Publisher and Sucsriber to RabbitMQ and tested sending and receiving messages. // Publisher ve Sucsriber için Console oluşturuldu ve RabbitMQ kütüphaneleri eklendi. Publisher ve Sucsriber’ın RabbitMQ’ya başarılı bir şekilde bağlayarak, mesaj gönderme ve alma işlemleri test edildi.

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Globalization;
+using System.Text;
+
+namespace RabbitMQ.publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://localhost:5672");
+
+           using var connection = factory.CreateConnection();
+
+            var chanell = connection.CreateModel();
+
+            chanell.QueueDeclare("hello-queue", true, false, false);
+
+            string message = "Hello World!";
+
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+            chanell.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+            Console.WriteLine("Mesaj gönderilmiştir");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://localhost:5672");
+
+            using var connection = factory.CreateConnection();
+
+            var chanell = connection.CreateModel();
+
+            //chanell.QueueDeclare("hello-queue", true, false, false);
+
+            var cunsormer = new EventingBasicConsumer(chanell);
+
+            chanell.BasicConsume("hello-queue", true, cunsormer);
+
+            cunsormer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Gelen Mesaj:"+message);
+            };
+
+            Console.ReadLine();
+        }
+
+    }
+}
